39ece02bb566bc0cab64d14326ff965ed4d72787
César Branco <72612483+ceszkraft@users.noreply.github.com>
2023-11-23 19:05:41 -0300

    about-us, footer fixed working

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, type Routes } from '@angular/router';
 import { FormComponent } from './components/form/form.component';
+import { AboutUsComponent } from './components/about-us/about-us.component';
 
 const routes: Routes = [
   {
     path: 'register', component: FormComponent
+  },
+  {
+    path: 'about', component: AboutUsComponent
   }
 ];
 
